3d4e342d9bcf61cc95e6a577e484f87e9d9aefec
Shefali Garg <shefali92@users.noreply.github.com>
2019-01-31 13:06:33 -0500

    RunCoreNLP.py file
    Script for running the coref resolution code.

---
+++ RunCoreNLP.py
@@ -0,0 +1,69 @@
+import shutil
+import os
+import sys
+import csv
+import stat
+import pandas as pd
+
+test_csv_dir = sys.argv[1]
+char_dir = sys.argv[2]
+output_dir = sys.argv[3]
+
+filenames = []
+fict_dict={}
+
+for filename in os.listdir(test_csv_dir):
+        if filename.endswith(".csv"):
+                filenames.append(filename)
+        else:
+                continue
+
+for filename in filenames:
+
+        this_text=''
+        fic_id = ''
+        chap_id = ''
+        for row in csv.DictReader(open(test_csv_dir+filename)):
+                if len(fic_id)==0:
+                        fic_id = row["fic_id"]
+                else:
+                        assert fic_id == row["fic_id"]
+
+                if len(chap_id)==0:
+                        chap_id = row["chapter_id"]
+                else:
+                        assert chap_id == row["chapter_id"]
+
+                this_text+=row["paragraph"]
+                this_text+=" # . "
+
+        # dump the txt file
+        print ("CoreNLP/"+filename[:-4])
+        with open("CoreNLP/"+filename[:-4],'w') as f:
+                f.write(this_text)
+        f.close()
+        os.chdir("CoreNLP")
+        os.system("./run.sh "+filename[:-4])
+        os.remove(filename[:-4])
+        os.chdir("..")
+        if (not os.path.isdir(char_dir)):
+                os.mkdir(char_dir)
+        if (not os.path.isdir(output_dir)):
+                os.mkdir(output_dir)
+        shutil.move("./CoreNLP/"+filename[:-4]+".chars" , char_dir+"/"+filename[:-4]+".chars")
+        shutil.move("./CoreNLP/"+filename[:-4]+".coref.out" ,output_dir+"/"+filename[:-4]+".coref.out")
+
+
+        f = open(output_dir+"/"+filename[:-4]+".coref.out")
+        fin = open(test_csv_dir+filename,"rb")
+        reader = csv.reader(fin)
+        fout = open(test_csv_dir+filename+".coref","wb")
+        writer = csv.writer(fout)
+        lines = f.readlines()
+        for row,text in zip(reader, lines):
+                row[len(row)-1]= text
+                writer.writerow(row)
+
+        fin.close()
+        f.close()
+        fout.close()                                                                                                                                                                                    1,1           Top
