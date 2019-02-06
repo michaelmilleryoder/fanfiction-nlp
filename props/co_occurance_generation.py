11f549e228abcb8301b9ad2155e25b65ee582813
Prashant Gupta <prashang@erebor.lti.cs.cmu.edu>
2019-02-06 18:05:20 -0500

    Reads the characters from a seperate file and accepts a custom output

--- props/co_occurance_generation.py
+++ props/co_occurance_generation.py
@@ -16,16 +16,22 @@ if len(sys.argv) < 2:
     print "Data path missing"
     exit(0)
 
+print "Loading NLP Model"
 nlp = spacy.load('en_core_web_sm')
 print "Model loaded"
 path = sys.argv[1]
-path = os.path.abspath(os.path.join(os.pardir, path))
+script_dir = os.path.dirname(__file__)  # <-- absolute dir the script is in
+
+rel_path = sys.argv[1]
+path = os.path.join(script_dir, rel_path)
+print "Reading file from " + path
+
 allFiles = glob.glob(path + "/*.csv")
 frame = pd.DataFrame()
 text = []
 count = 0
 if len(allFiles) < 1:
-    print "No files found at the location provided"
+    print "No files found at" + path
     exit(0)
 for file in allFiles:
     df = pd.read_csv(file, index_col=None, header=0)
@@ -35,17 +41,34 @@ for file in allFiles:
 character_dict = {}
 text = ''.join(text)
 
+# Read all files for characters
+
+rel_path = sys.argv[3]
+char_path = os.path.join(script_dir, rel_path)
+print "Reading characters from " + path
+charFiles = glob.glob(char_path + "/*")
+
+if len(charFiles) < 1:
+    print "No character files found at" + char_path
+    exit(0)
+
+# Store the characters in a dict with normal names as keys
+for file in charFiles:
+    chars = open(file).readlines()
+    for char in chars:
+        character_dict[char[char.find('_') + 1: char.find(')')].lower()] = char.strip('\n')
+
 
 processed_text = []
 for word in text.split(" "):
     if word.startswith("($_"):
-        character_dict[word[word.find('_') + 1: word.find(')')].lower()] = word
+        # character_dict[word[word.find('_') + 1: word.find(')')].lower()] = word
         processed_text.pop()
         word = word[word.find('_') + 1: word.find(')')]
 
     processed_text.append(word)
 processed_text = ' '.join(processed_text)
-print "before nlp"
+
 doc = nlp(unicode(processed_text))
 
 data_list = []
@@ -105,6 +128,9 @@ for key in new_char_dict:
         if (value != 0):
             counter_dict[key1] = value
     sorted_dict[key] = counter_dict
+
+rel_path = sys.argv[2]
+output_path = os.path.join(script_dir, rel_path)
 
 with open('result.json', 'w') as fp:
     json.dump(sorted_dict, fp)
