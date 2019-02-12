790d187820010b434b1089b482ff346fac2c7611
Prashant Gupta <prashang@misty.lti.cs.cmu.edu>
2019-02-12 14:50:28 -0500

    Creates 2 files per fic, adjectives and relations

--- props/co_occurance_generation.py
+++ props/co_occurance_generation.py
@@ -69,6 +69,73 @@ def adj_matrix(character_dict, data_list):
     return chars
 
 
+def ship_matrix(character_dict, data_list):
+    chars = {}
+    index = 0
+    ngram = 8
+    for row in data_list:
+        # print row['text']
+        if row['text'] in character_dict:
+            if character_dict[row["text"]] in chars:
+                for i in range(1, ngram + 1):
+                    if data_list[index + i]['text'] in character_dict and data_list[index + i]['text'] != row['text']:
+                        chars[character_dict[row["text"]]].update({character_dict[data_list[index + i]['text']]: 1})
+                    if data_list[index - i]['text'] in character_dict and data_list[index - i]['text'] != row['text']:
+                        chars[character_dict[row["text"]]].update({character_dict[data_list[index - i]['text']]: 1})
+            else:
+                chars[character_dict[row["text"]]] = Counter()
+                for i in range(1, ngram + 1):
+                    if data_list[index + i]['text'] in character_dict and data_list[index + i]['text'] != row['text']:
+                        chars[character_dict[row["text"]]].update({character_dict[data_list[index + i]['text']]: 1})
+                    if data_list[index - i]['text'] in character_dict and data_list[index - i]['text'] != row['text']:
+                        chars[character_dict[row["text"]]].update({character_dict[data_list[index - i]['text']]: 1})
+        index += 1
+    return chars
+
+
+def creating_cooccurence(adj_chars):
+    adj_frame = pd.DataFrame.from_dict(adj_chars, orient='index').reset_index().fillna(0)
+    col_names = list(adj_frame.columns.values)
+    col_names = col_names[1:]
+    char_list = []
+    # Converting term counts to TFIDF
+    for char in adj_frame["index"]:
+        char_list.append(char)
+    adj_frame = adj_frame.drop(["index"], axis=1)
+    return adj_frame, char_list, col_names
+
+
+def TFIDF_construct(data_frame, char_list, col_names):
+    tfidf = TfidfTransformer(norm='l2', use_idf=True, smooth_idf=True, sublinear_tf=False)
+    mat = tfidf.fit_transform(data_frame).toarray()
+    data_frame = pd.DataFrame(mat)
+    data_frame.index = char_list
+    data_frame.columns = col_names
+
+    new_char_dict = {}
+    char_dict = data_frame.to_dict('index')
+    for key in char_dict:
+        new_char_dict[key] = char_dict[key]
+    return new_char_dict
+
+
+def create_ordered_dict(char_dict):
+    sorted_dict = OrderedDict()
+    for key in char_dict:
+        counter_dict = OrderedDict()
+        for key1, value in sorted(char_dict[key].iteritems(), key=lambda (k, v): (v, k), reverse=True):
+            if (value != 0):
+                counter_dict[key1] = value
+        sorted_dict[key] = counter_dict
+    return sorted_dict
+
+
+def write_to_json(output_path, sorted_dict):
+    print "Writing JSON to " + output_path
+    with open(output_path, 'w') as fp:
+        json.dump(sorted_dict, fp)
+
+
 print "Loading NLP Model"
 nlp = spacy.load('en_core_web_sm')
 print "Model loaded"
@@ -121,39 +188,22 @@ for file in allFiles:
 
     df = pd.DataFrame(data_list)
 
-    chars = adj_matrix(character_dict, data_list)
-    print "Creating co-occurence"
-    dfnew = pd.DataFrame.from_dict(chars, orient='index').reset_index().fillna(0)
-    col_names = list(dfnew.columns.values)
-    col_names = col_names[1:]
-    char_list = []
-    # Converting term counts to TFIDF
-    for char in dfnew["index"]:
-        char_list.append(char)
-    dfnew = dfnew.drop(["index"], axis=1)
+    adj_chars = adj_matrix(character_dict, data_list)
+    ship_chars = ship_matrix(character_dict, data_list)
 
-    tfidf = TfidfTransformer(norm='l2', use_idf=True, smooth_idf=True, sublinear_tf=False)
-    mat = tfidf.fit_transform(dfnew).toarray()
-    dffinal = pd.DataFrame(mat)
-    dffinal.index = char_list
-    dffinal.columns = col_names
+    print "Creating co-occurence for ", file.split('/')[-1].split('.')[0]
+    adj_frame, adj_char_list, adj_col_names = creating_cooccurence(adj_chars)
+    ship_frame, ship_char_list, shipcol_names = creating_cooccurence(ship_chars)
 
-    new_char_dict = {}
-    char_dict = dffinal.to_dict('index')
-    for key in char_dict:
-        new_char_dict[key] = char_dict[key]
+    adj_char_dict = TFIDF_construct(adj_frame, adj_char_list, adj_col_names)
+    ship_char_dict = TFIDF_construct(ship_frame, ship_char_list, shipcol_names)
 
-    sorted_dict = OrderedDict()
-    for key in new_char_dict:
-        counter_dict = OrderedDict()
-        for key1, value in sorted(new_char_dict[key].iteritems(), key=lambda (k, v): (v, k), reverse=True):
-            if (value != 0):
-                counter_dict[key1] = value
-        sorted_dict[key] = counter_dict
+    adj_sorted_dict = create_ordered_dict(adj_char_dict)
+    ship_sorted_dict = create_ordered_dict(ship_char_dict)
 
     rel_path = os.path.join(sys.argv[2], '')
-    output_path = os.path.join(script_dir, rel_path) + file.split('/')[-1].split('.')[0] + "_cooccurrence.json"
+    adj_output_path = os.path.join(script_dir, rel_path) + file.split('/')[-1].split('.')[0] + "_adj_cooccurrence.json"
+    ship_output_path = os.path.join(script_dir, rel_path) + file.split('/')[-1].split('.')[0] + "_ship_cooccurrence.json"
 
-    print "Writing JSON to " + output_path
-    with open(output_path, 'w') as fp:
-        json.dump(sorted_dict, fp)
+    write_to_json(adj_output_path, adj_sorted_dict)
+    write_to_json(ship_output_path, ship_sorted_dict)
